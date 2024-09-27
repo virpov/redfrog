b8af11e0be222701a4b8a01bc13f403ddbbc118c
virgfern <virgfern@student.42luxembourg.lu>
2024-09-27 21:26:56 +0200

    fixing version

--- scripts/npc_quest.gd
+++ scripts/npc_quest.gd
@@ -9,11 +9,11 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 
 func start_dialog():
-	Dialogic.timeline_ended.connect(_on_timeline_ended2)
+	Dialogic.timeline_ended.connect(_on_timeline_ended)
 	Dialogic.start("questNorminette")
 	get_viewport().set_input_as_handled()
 
-func _on_timeline_ended2():
-	Dialogic.timeline_ended.disconnect(_on_timeline_ended2)
+func _on_timeline_ended():
+	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
 	$Area2D/CollisionShape2D.queue_free()
 	Global.unfreeze_player()
